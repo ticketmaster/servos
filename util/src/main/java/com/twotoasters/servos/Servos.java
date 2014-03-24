3f5765d5126f5a333c02a4a6d3d0112bec2d22a0
Carlton Whitehead <carlton@twotoasters.com>
2014-03-24 14:56:14 -0400

    Clarify Application Context usage in Javadocs

--- util/src/main/java/com/twotoasters/servos/Servos.java
+++ util/src/main/java/com/twotoasters/servos/Servos.java
@@ -17,12 +17,23 @@ public class Servos {
      *         Servos.init(this);
      *     }
      * }
-     * @param application the Application which Servos will use to pass a Context
+     * @param application the Application which Servos will use to obtain a reference to an
+     *                    Application Context
      */
     public static void init(Application application) {
         Servos.application = application;
     }
 
+    /**
+     * Get a reference to the Application
+     *
+     * Servos helper methods can obtain a reference to the Application instance, which
+     * is a Context. It should only be used where it is safe to use an Application instance as a
+     * Context, and where no additional flexibility could be gained by allowing the user to pass
+     * their own Context.
+     *
+     * @return Application Context
+     */
     public static Application getApplication() {
         if (application == null) {
             throw new IllegalStateException("Programmer error: must first call Servos.init(this) from your Application.onCreate()");
