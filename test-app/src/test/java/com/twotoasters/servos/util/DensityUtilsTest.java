19c813c5d1f96fecb9a6c24091a45016b279742b
Curtis Martin <curtis@twotoasters.com>
2014-10-09 13:57:34 -0400

    Add tests

---
+++ test-app/src/test/java/com/twotoasters/servos/util/DensityUtilsTest.java
@@ -0,0 +1,29 @@
+package com.twotoasters.servos.util;
+
+import android.content.res.Resources;
+
+import com.twotoasters.servos.ServosRobolectricTestRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(ServosRobolectricTestRunner.class)
+public class DensityUtilsTest {
+
+    private final int DP_CONSTANT = 160; //dp is based on density of a 160 dpi screen
+
+    @Test
+    public void itShouldCorrectlyCalculateDpFromPixels() {
+        int dpi = Resources.getSystem().getDisplayMetrics().densityDpi;
+        assertThat(DensityUtils.getDpFromPixels(1)).isEqualTo(1 / (dpi / DP_CONSTANT));
+    }
+
+    @Test
+    public void itShouldCorrectlyCalculatePixelsFromDp() {
+        int dpi = Resources.getSystem().getDisplayMetrics().densityDpi;
+        assertThat(DensityUtils.getPixelsFromDp(1)).isEqualTo(dpi / DP_CONSTANT);
+    }
+
+}
